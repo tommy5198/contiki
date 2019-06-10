767925b399caa21bd65be14458ddc48705bf7878
tommy5198 <tommy5198@gmail.com>
2019-06-10 10:16:48 +0200

    add debug print

--- examples/ipv6/rpl-collect/udp-sender_danger.c
+++ examples/ipv6/rpl-collect/udp-sender_danger.c
@@ -81,6 +81,7 @@ collect_common_net_print(void)
       r != NULL;
       r = uip_ds6_route_next(r)) {
     PRINT6ADDR(&r->ipaddr);
+    PRINTF(" ");
   }
   PRINTF("---\n");
 }
@@ -143,9 +144,12 @@ collect_common_send(void)
         parent.u8[LINKADDR_SIZE - 2] = nbr->ipaddr.u8[sizeof(uip_ipaddr_t) - 1];
         parent_etx = rpl_get_parent_rank((uip_lladdr_t *) uip_ds6_nbr_get_ll(nbr)) / 2;
       }
+/*
       PRINTF("Current Preferred parent: ");
       PRINT6ADDR(rpl_get_parent_ipaddr(preferred_parent));
       PRINTF("\n");
+*/
+      collect_common_net_print();
     }
     rtmetric = dag->rank;
     beacon_interval = (uint16_t) ((2L << dag->instance->dio_intcurrent) / 1000);
